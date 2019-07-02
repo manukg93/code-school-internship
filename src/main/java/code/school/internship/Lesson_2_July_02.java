e2ea0e01fdc2f891ece3913acd6638f5e753f1ff
aarshakyans <yonjlax-->
2019-07-02 11:46:42 -0700

    L1

--- src/main/java/code/school/internship/Lesson_2_July_02.java
+++ src/main/java/code/school/internship/Lesson_2_July_02.java
@@ -1,6 +1,17 @@
 package code.school.internship;
 
+import java.util.Scanner;
+
 public class Lesson_2_July_02 {
+    public static void main(String[] args) {
+
+    int a;
+        Scanner input = new Scanner(System.in);
+        a=input.nextInt();
+        System.out.println(a);
+
+
+    }
 
 
 }
