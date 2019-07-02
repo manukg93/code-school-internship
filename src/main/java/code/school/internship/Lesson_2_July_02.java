dc0d258c639fe14c5cc000f0f978129383dd4483
Hamo <hmosargsyan888@gmail.com>
2019-07-02 22:43:48 +0400

    lesson 2  02.07.2019

--- src/main/java/code/school/internship/Lesson_2_July_02.java
+++ src/main/java/code/school/internship/Lesson_2_July_02.java
@@ -1,6 +1,19 @@
 package code.school.internship;
 
+
 public class Lesson_2_July_02 {
+    public static void main(String[] args) {
+        double pi, r, a;
+        r = 10.8; // radius of circle
+
+        pi = 3.1416; // pi, approximately
+
+        a = pi * r * r; // compute area
+
+        System.out.println("Area of circle is " + a);
+
+        }
+    }
 
 
-}
+
