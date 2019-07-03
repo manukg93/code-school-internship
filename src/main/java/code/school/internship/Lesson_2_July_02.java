a8292fc5980d9e3053e6691ab0b61e429f3485fd
gexayr <gexo7777@gmail.com>
2019-07-03 23:48:03 +0400

    test

--- src/main/java/code/school/internship/Lesson_2_July_02.java
+++ src/main/java/code/school/internship/Lesson_2_July_02.java
@@ -1,6 +1,8 @@
-package code.school.internship;
+package main.java.code.school.internship;
 
 public class Lesson_2_July_02 {
-
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
 
 }
