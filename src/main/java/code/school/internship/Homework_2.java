789f54240a4863ff3aed03de74d427b2fe8155fd
Mane Zakaryan <manezak94@gmail.com>
2019-07-06 14:43:28 +0500

    Homeworks 06/07/2019 test

--- src/main/java/code/school/internship/Homework_2.java
+++ src/main/java/code/school/internship/Homework_2.java
@@ -4,7 +4,7 @@ public class Homework_2 {
     public static void main(String[] args){
         // the rectangle area
         int x = 55, y = 100;
-        System.out.println("The area of rectagle is equal to its width and height multiplication");
+        System.out.println("The area of rectangle is equal to its width and height multiplication");
         System.out.println("Area = x * y ");
         System.out.println("z = " + (x * y));
     }
