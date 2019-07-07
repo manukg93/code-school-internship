865f36170126c88c039b85a43bcd6fd1c2cef5fd
Hamo <hmosargsyan888@gmail.com>
2019-07-07 21:29:48 +0400

    lesson3 homework

---
+++ src/main/java/code/school/internship/Xndir_13.java
@@ -0,0 +1,19 @@
+package code.school.internship;
+
+public class Xndir_13 {
+    public static void main(String[] args) {
+        float n=1111111;
+        if(n<0||n>0){
+            System.out.println("positive or negative");
+        } else{
+            System.out.println("zero");
+        }
+        if(n<1){
+            System.out.println("small");
+            }
+        if(n>1000000){
+            System.out.println("large");
+        }
+
+    }
+}
