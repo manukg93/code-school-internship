bc344aa30f0df7ac0c8a2ca9d4c8aa48f79eb5be
haykserobyan <89manukyan>
2019-07-09 02:59:58 +0400

    Homework 3 exercises 17,18,19,20,21-???

--- src/main/java/code/school/internship/OddOrEven.java
+++ src/main/java/code/school/internship/OddOrEven.java
@@ -1,4 +1,11 @@
 package code.school.internship;
 
 public class OddOrEven {
+    public static void main(String[] args) {
+        int a=19;
+        if ( a%2==0 )
+            System.out.println("The number is even");
+        else
+            System.out.println("The number is odd");
+    }
 }
---
+++ src/main/java/code/school/internship/While.java
@@ -0,0 +1,8 @@
+package code.school.internship;
+
+public class While {
+    public static void main(String[] args) {
+
+
+    }
+}
