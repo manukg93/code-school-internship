15155f3edc7aec529406e5069d7a01d92fbccd3c
Ovsanna <ovsannas@gmail.com>
2019-07-08 18:06:58 +0400

    Lesson3-homework-exercises1-5

---
+++ src/main/java/code/school/internship/Ex5Series.java
@@ -0,0 +1,14 @@
+package code.school.internship;
+
+public class Ex5Series {
+    public static void main(String[] args) {
+        final int N = 3;
+        double sum = 1;
+
+        for (int i = 2; i <= N; i++) {
+            sum += (double) 1/i;
+
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/code/school/internship/OddSum1.java
@@ -0,0 +1,19 @@
+package code.school.internship;
+
+//
+public class OddSum1 {
+    public static void main(String[] args) {
+        final long N = 1_000_000;
+        int n = 1;
+        long sum = 0;
+
+        do {
+            sum += n;
+            n += 2;
+        } while (n < N);
+
+        System.out.println("Sum of odd numbers from 1 to " + N + " is " + sum);
+
+
+    }
+}
