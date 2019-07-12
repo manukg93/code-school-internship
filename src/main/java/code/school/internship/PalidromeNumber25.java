b3cdfac1d65876e991a3401613a25a393acb54e1
Hamo <hmosargsyan888@gmail.com>
2019-07-12 18:44:28 +0400

    add gitignor

---
+++ src/main/java/code/school/internship/PalidromeNumber25.java
@@ -0,0 +1,21 @@
+package code.school.internship;
+
+public class PalidromeNumber25 {
+    public static void main(String[] args) {
+        int x = 123000321;
+        int revers = 0;
+        int temp = x;
+        while (x != 0) {
+            int reminder = x % 10;
+            revers = revers * 10 + reminder;
+            x = x / 10;
+        }
+        if (temp  == revers) {
+            System.out.println("The number i palindrome");
+
+        }else
+        System.out.println("The number is not palindrome");
+
+    }
+
+}
