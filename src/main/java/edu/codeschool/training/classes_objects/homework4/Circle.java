805e5df0b33a7fbedde0c73b4a49385b00d4fe2a
Mane Zakaryan <manezak94@gmail.com>
2019-07-16 09:04:53 +0500

    Circle homework 1.1

---
+++ src/main/java/edu/codeschool/training/classes_objects/homework4/Circle.java
@@ -0,0 +1,20 @@
+package edu.codeschool.training.classes_objects.homework4;
+
+public class Circle {
+    private double radius=1.0;
+    private String color="red";
+    public Circle(double r) {
+        radius = r;
+        color = "red";
+    }
+    public double getradius() {
+        return radius;
+    }
+    public double getarea() {
+        return radius*radius*Math.PI;
+    }
+    public String getcolor () {
+        return color;
+    }
+}
+
