657f849dfc3141b089d7002e048beac18cebe96f
Mane Zakaryan <manezak94@gmail.com>
2019-08-06 09:57:03 +0500

    Homework 9

---
+++ src/main/java/Homework10/City.java
@@ -0,0 +1,40 @@
+package Homework10;
+
+public class City implements Cloneable {
+    String name;
+    long countOfResidents;
+    double surface;
+City(String name, long countOfResidents, double surface) {
+    this.name=name;
+    this.countOfResidents=countOfResidents;
+    this.surface=surface;
+}
+    public String getName() {
+        return name;
+    }
+    public long getCountOfResidents() {
+        return countOfResidents;
+    }
+    public double getSurface() {
+        return surface;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCountOfResidents(long countOfResidents) {
+        this.countOfResidents = countOfResidents;
+    }
+    public void setSurface(double surface) {
+        this.surface = surface;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + countOfResidents + " " + surface;
+    }
+
+  @Override
+ protected Object clone() throws CloneNotSupportedException {
+       return super.clone();
+  }
+}
---
+++ src/main/java/Homework10/Country.java
@@ -0,0 +1,38 @@
+package Homework10;
+
+public class Country implements Cloneable {
+    String name;
+    String nationality;
+    City capital;
+    Province[] province;
+Country() {
+this.name="Mane Zakaryan";
+this.nationality="Armenian";
+ this.capital=null;
+ this.province=null;
+}
+Country(String name, String nationality, City City, Province[] province){
+    this.nationality=nationality;
+    this.name=name;
+    this.capital=capital;
+    this.province=province;
+}
+    public String getName() { return name; }
+    public String getNationality(){return nationality;}
+    public City getCapital(){ return capital;}
+    public Province[] getProvince() { return province; }
+    public void setName(String name){ this.name=name;}
+    public void setNationality(String nationality){ this.nationality=nationality;}
+    public void setCapital(City capital){this.capital=capital;}
+    public void setProvince(Province[] province){this.province=province;}
+
+    @Override
+    public String toString() {
+        return name + " " + nationality + " " + capital + " " + province;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return (Country)super.clone();
+    }
+}
