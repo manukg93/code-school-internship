2f42bfcc408c6b033b963fc48b1d99d8f7592660
aarshakyans <yonjlax-->
2019-08-12 07:22:07 -0700

    homework 11

--- src/main/java/homework11/ex1/ClassCastExceptionDemo.java
+++ src/main/java/homework11/ex1/ClassCastExceptionDemo.java
@@ -3,7 +3,8 @@ package homework11.ex1;
 public class ClassCastExceptionDemo {
     public static void main(String[] args) {
         try {
-
+            Object i = Integer.valueOf(42);
+            String s = (String)i;
 
         }catch (ClassCastException ex){
             System.out.println(ex.getMessage());
