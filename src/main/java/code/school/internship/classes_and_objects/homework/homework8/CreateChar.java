414f66fb79129d2b520457e65f613f5008efc45e
vladimir.agayan@mail.ru <Maxx8891>
2019-08-12 18:11:44 +0400

    Lesson 2 and 3 and 4 and 5  6,7,8,9,10 home work

--- src/main/java/code/school/internship/classes_and_objects/homework/homework8/CreateChar.java
+++ src/main/java/code/school/internship/classes_and_objects/homework/homework8/CreateChar.java
@@ -1,6 +1,6 @@
 package code.school.internship.classes_and_objects.homework.homework8;
 
-// import edu.codeschool.training.classes_objects.homework8.exercise2.CustomString;
+// import code.school.internship.classes_and_objects.homework.homework8.CustomString;
 
 public class CreateChar {
     public static void main(String[] args) {
