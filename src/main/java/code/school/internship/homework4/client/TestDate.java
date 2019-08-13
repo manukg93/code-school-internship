d0f385ff27014ee94b05d297a40297faf2dff7d4
sargi <sargiss@gmail.com>
2019-08-13 20:06:49 +0400

    updated homework 4

--- src/main/java/code/school/internship/homework4/client/TestDate.java
+++ src/main/java/code/school/internship/homework4/client/TestDate.java
@@ -6,7 +6,7 @@ import code.school.internship.homework4.Date;
 public class TestDate {
     public static void main(String[] args) {
         Date date = new Date();
-        date.toString(1992,10,9);
+        date.toString(1993,10,9);
         date.plusDay(25);
         date.plusYear(5);
 
