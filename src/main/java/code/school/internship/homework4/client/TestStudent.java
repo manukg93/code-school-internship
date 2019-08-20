86e3c9e9f7acc02e3652f271a4f90cb887b27e39
sargi <sargiss@gmail.com>
2019-08-20 14:48:53 +0400

    homework 5

---
+++ src/main/java/code/school/internship/homework4/client/TestStudent.java
@@ -0,0 +1,18 @@
+package code.school.internship.homework4.client;
+
+import code.school.internship.homework4.Student;
+
+
+public class TestStudent {
+    public static void main(String[] args) {
+
+        Student jon = new Student("Jon",22);
+        Student jon2 = new Student("jon2", 22);
+        Student jon3 = new Student("jon3",24);
+jon.numberObj();
+
+
+    }
+
+
+}
