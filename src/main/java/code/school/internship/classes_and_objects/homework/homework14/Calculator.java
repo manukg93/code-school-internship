7f9ddf9c1666524482ba3f7f2eb4ee4e303431cf
vladimir.agayan@mail.ru <Maxx8891>
2019-08-24 13:54:29 +0400

    Lesson 2 and 3 and 4 and 5  6,7,8,9,10 11 12 13 14 home work

---
+++ src/main/java/code/school/internship/classes_and_objects/homework/homework14/Calculator.java
@@ -0,0 +1,62 @@
+package code.school.internship.classes_and_objects.homework.homework14;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Start ?");
+            System.out.println("Exit ?");
+            String start = input.next();
+            if (start.equals("exit")) {
+                System.exit(0);
+            }
+            try {
+                System.out.print("one int=");
+                int firstInt = input.nextInt();
+                System.out.print("second int=");
+                int secondInt = input.nextInt();
+                System.out.print("input Operator` + , - , * , /");
+                char operator = input.next().charAt(0);
+
+
+                try {
+
+                    if (operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+                        if (operator == '+') {
+                            System.out.println(firstInt + secondInt);
+                        } else if (operator == '-') {
+                            System.out.println(firstInt - secondInt);
+                        } else if (operator == '*') {
+                            System.out.println(firstInt * secondInt);
+                        } else if (operator == '/') {
+                            try {
+                                if (secondInt != 0) {
+                                    System.out.println(firstInt / secondInt);
+                                } else {
+                                    throw new ArithmeticException("/ by zero");
+                                }
+                            } catch (ArithmeticException ex) {
+                                System.out.println(ex);
+                                continue;
+                            }
+                        }
+
+                    } else {
+                        throw new IllegalOperatorException("Operator is illegal");
+                    }
+                } catch (IllegalOperatorException ex) {
+                    System.out.println(ex);
+                    continue;
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println(ex);
+                continue;
+            }
+        }
+
+
+    }
+}
---
+++ src/main/java/code/school/internship/classes_and_objects/homework/homework14/IllegalOperatorException.java
@@ -0,0 +1,6 @@
+package code.school.internship.classes_and_objects.homework.homework14;
+
+public class IllegalOperatorException extends Throwable {
+    public IllegalOperatorException(String operator_is_illegal) {
+    }
+}
