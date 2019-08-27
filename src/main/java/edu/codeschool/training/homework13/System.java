ee275eb00ad4f78cd4728e6b8b38973d69f18039
Ovsanna <ovsannas@gmail.com>
2019-08-27 15:56:35 +0400

    homework 13 - task 4. System class

---
+++ src/main/java/edu/codeschool/training/homework13/System.java
@@ -0,0 +1,73 @@
+package edu.codeschool.training.homework13;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class System {
+
+    // static fields
+    private static File sout = null;
+    private static PrintStream out = null;
+
+    // private method
+    private static void createOutputFile() throws IOException {
+
+        sout = new File(java.lang.System.getProperty("user.dir") + File.separator + "src" + File.separator
+                + "main" + File.separator + "java" + File.separator + "resources" + File.separator + "sout.txt");
+
+        if (!(sout.exists())) {
+
+            sout.createNewFile();
+
+        }
+    }
+
+    // private method
+    private static void createOutputObject() throws IOException {
+
+        out = new PrintStream(sout);
+
+    }
+
+    public static void print(String arg) throws IOException {
+
+        try {
+
+            createOutputFile();
+            createOutputObject();
+
+
+            out.print(arg);
+
+        } catch (FileNotFoundException fnf) {
+
+            System.out.println("file not found exception: " + fnf.getStackTrace());
+
+        } catch (IOException ioe) {
+
+            System.out.println("I/O exception: " + ioe.getStackTrace());
+        }
+    }
+
+    public static void println(String arg) throws IOException {
+
+        try {
+
+            createOutputFile();
+            createOutputObject();
+
+            out.println(arg);
+
+        } catch (FileNotFoundException fnf) {
+
+            System.out.println("file not found exception: " + fnf.getStackTrace());
+
+        } catch (IOException ioe) {
+
+            System.out.println("I/O exception: " + ioe.getStackTrace());
+        }
+
+    }
+}
---
+++ src/main/java/edu/codeschool/training/homework13/TestSystem.java
@@ -0,0 +1,13 @@
+package edu.codeschool.training.homework13;
+
+import java.io.IOException;
+
+public class TestSystem {
+
+    public static void main(String[] args) throws IOException {
+
+        System.print("Hello, world");
+
+        System.println("Armenia");
+    }
+}
