00595dfb575ce26f0ff542e4a35cc411fba4ed76
haykserobyan <89manukyan>
2019-08-31 15:22:17 +0400

    Homework 16 ex. 1 (subArray update)

--- src/main/java/edu/codeschool/training/classes_objects/homework_16_Generics/Ex_1/Math.java
+++ src/main/java/edu/codeschool/training/classes_objects/homework_16_Generics/Ex_1/Math.java
@@ -1,8 +1,21 @@
 package edu.codeschool.training.classes_objects.homework_16_Generics.Ex_1;
 
-import java.util.Arrays;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
 
-public class Math <E> {
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class Math <E extends Comparable> {
+    private E[] list;
+    private int size;
+
+
+    public Math(E[] list, int size) {
+        this.size = list.length;
+    }
 
     public static <E extends Comparable<E>> E max(E[] list) {
         E max = list[0];
@@ -35,44 +48,41 @@ public class Math <E> {
                 }
 
             }
-        }System.out.println(Arrays.toString(list));
+        }
+        System.out.println(Arrays.toString(list));
 
     }
 
-    public static <E extends Comparable<E>> void reverseList(E[] list){
-        for (int i = 0; i < list.length/2; i++){
-            E tmp=list[i];
+    public static <E extends Comparable<E>> void reverseList(E[] list) {
+        for (int i = 0; i < list.length / 2; i++) {
+            E tmp = list[i];
             list[i] = list[list.length - i - 1];
-            list[list.length - i - 1]=tmp;
+            list[list.length - i - 1] = tmp;
         }
         System.out.println(Arrays.toString(list));
     }
 
-    public static <E extends Comparable<E>> void searchElementContain(E[] list, E element){
-        for (int i = 1; i < list.length; i++){
-            if(list[i].compareTo(element)==0){
-                System.out.println("The array contain the element : "+ element);
+    public static <E extends Comparable<E>> void searchElementContain(E[] list, E element) {
+        for (int i = 1; i < list.length; i++) {
+            if (list[i].compareTo(element) == 0) {
+                System.out.println("The array contain the element : " + element);
                 return;
-            }else {
-                System.out.println("The array don't contain the element : "+ element);
+            } else {
+                System.out.println("The array don't contain the element : " + element);
                 return;
             }
         }
     }
 
-    public static <E extends Comparable<E>, len> void subArray(E[] array, int offset ,int len){
-        System.out.print("[");
-        for (int i = offset-1; i <array.length-1; i++){
-            if(i<len-1){
-                System.out.print(array[i]);
-                System.out.print(" , ");
-                continue;
-            }if(i<len){
-                System.out.print(array[i]);
-            } else break;
-
+    public static <E extends Comparable<E>> E[] subArray(E[] list, int offset, int len) {
+        E[] subArray = (E[]) new Comparable[len];
+        if (offset >= 0 && len >= 0 && len <= (list.length - offset)) {
+            int temp=0;
+            for (int i = offset; i <= len-1; i++) {
+                subArray[temp] = list[i];
+                temp++;
+            }
         }
-        System.out.print("]\n");
+        return subArray;
     }
-
 }
