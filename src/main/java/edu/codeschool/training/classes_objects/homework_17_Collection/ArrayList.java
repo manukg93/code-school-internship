cafeb9f6e123d6221c7a36e763cefc195ace0059
haykserobyan <89manukyan>
2019-09-08 23:43:57 +0400

    Homework 17  ArrayList

--- src/main/java/edu/codeschool/training/classes_objects/homework_17_Collection/ArrayList.java
+++ src/main/java/edu/codeschool/training/classes_objects/homework_17_Collection/ArrayList.java
@@ -6,7 +6,7 @@ import java.util.function.UnaryOperator;
 
 public class ArrayList<T> implements List<T>,Iterable<T> {
     int index=0;
-    T[] arrayList=(T[]) new Object[10];
+    T[] arrayList=(T[]) new Object[5];
 
     @Override
     public int size() {
@@ -33,12 +33,13 @@ public class ArrayList<T> implements List<T>,Iterable<T> {
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
-            private int current;
-            private T[] iterator;
+            private int current=0;
+            private T[] iterator = arrayList;
+
 
             @Override
             public boolean hasNext() {
-                return (current < iterator.length);
+                return (current < size());
             }
 
             @Override
@@ -60,51 +61,47 @@ public class ArrayList<T> implements List<T>,Iterable<T> {
 
     @Override
     public <T1> T1[] toArray(T1[] a) {
-        if(a.length>=size()){
-            for (int i=0;i<size();i++){
-                a[i]=(T1)arrayList[i];
-            }
-            return a;
-        }else {
-            T1[] newA = (T1[]) new Object[size()];
-            for (int i = 0; i < size(); i++) {
-                newA[i] = (T1) arrayList[i];
-            }
-            return newA;
-        }
+
+        return a;
     }
 
     @Override
     public boolean add(T t) {
-        arrayList[index] = t;
-        index++;
-        if (index >= arrayList.length) {
-            T[] newArrayList = (T[]) new Object[index];
-            for (int i = 0; i < arrayList.length; i++) {
-                newArrayList[i] = arrayList[i];
+        if(index<arrayList.length){
+            arrayList[index]=t;
+            index++;
+            return true;
+        }else{
+            T [] addedArrayList=(T[]) new Object[size()+1];
+            for(int i=0;i<size();i++){
+                addedArrayList[i]=arrayList[i];
             }
-
-            this.arrayList = newArrayList;
+            addedArrayList[arrayList.length]=t;
+            this.arrayList=addedArrayList;
+            index++;
+            return true;
         }
-        arrayList[index] = t;
-        return true;
     }
 
     @Override
     public boolean remove(Object o) {
         for(int i=0;i<arrayList.length;i++){
             if(arrayList[i].equals(o)){
-                T[] removedArrayList=(T[])new Object[arrayList.length-1];
-                for(int j=0;j<i;j++){
-                    removedArrayList[j]=arrayList[i];
-                }
                 for(int j=i;j<arrayList.length-1;j++){
-                    removedArrayList[j]=arrayList[i+1];
+                    arrayList[j] = arrayList[j + 1];
                 }
-                this.arrayList = removedArrayList;
-                return true;
+                T [] removedArrayList=(T[])new Object[size()-1];
+                for(int k=0;k<removedArrayList.length;k++){
+                    removedArrayList[k]=arrayList[k];
+                }
+                this.arrayList=removedArrayList;
+                index--;
             }
-        }return false;
+        }
+
+
+        return true;
+
     }
 
     @Override
@@ -117,95 +114,151 @@ public class ArrayList<T> implements List<T>,Iterable<T> {
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        T[] addedArray=(T[])new Object[c.size()+size()];
-        toArray(addedArray);
-        for(int i=size();i<(c.size()+size());i++){
-            for (Object el : c){
-                addedArray[i]=(T)el;
-            }
+        T[] addedArray = (T[]) new Object[c.size() + size()];
+        for (int i = 0; i < size(); i++) {
+            addedArray[i] = arrayList[i];
         }
-        this.arrayList=addedArray;
+        Iterator<? extends T> iterator = c.iterator();
+        while(iterator.hasNext())
+            for (int i = size(); i < addedArray.length; i++){
+                addedArray[i]=iterator.next();
+
+        }
+        this.arrayList = addedArray;
+        this.index = c.size() + size();
         return true;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends T> c) {
-        return false;
-    }
+        T[] addedArray = (T[]) new Object[c.size() + size()];
+        Iterator<? extends T> iterator = c.iterator();
+        for (int i = 0; i < index; i++) {
+            addedArray[i] = arrayList[i];
+        }
+        while(iterator.hasNext())
+            for (int i = index; i < size(); i++){
+                addedArray[i]=iterator.next();
 
+            }
+        for(int i=(c.size()+index);i<addedArray.length;i++){
+            addedArray[i]=arrayList[index++];
+        }
+        this.arrayList=addedArray;
+        this.index=c.size() + size();
+        return true;
+    }
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        Iterator<?> iterator = c.iterator();
+        for(T el:arrayList){
+            while(iterator.hasNext()){
+                el=(T)iterator.next();
+                remove(el);
+            }return true;
+        }
+      return false ;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
+        //??????????????
         return false;
     }
 
     @Override
     public void clear() {
+        for (T el :arrayList) {
+            remove(0);
+        }
+        index--;
 
     }
 
     @Override
     public T get(int index) {
-        return null;
+        int num=-1;
+        if(index<0 || index>arrayList.length){
+            throw new ArrayIndexOutOfBoundsException("Index is out of bounds: "    + index);
+        }
+        else
+            return arrayList[index];
     }
 
     @Override
     public T set(int index, T element) {
-        return null;
+        T oldElement = get(index);
+        arrayList[index] = element;
+        return oldElement;
     }
 
     @Override
     public void add(int index, T element) {
+        if (index < 0 || index > size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        add(element);
 
+        for (int i=size()-1; i>index; i--) {
+            arrayList[i] = arrayList[i-1];
+        }
+        arrayList[index] = element;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        T element = get(index);
+        for (int i=index; i<size()-1; i++) {
+            arrayList[i] = arrayList[i+1];
+
+        }
+        remove(arrayList[arrayList.length-1]);
+        index--;
+        return element;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i<size(); i++) {
+            if (o.equals(arrayList[i])) {
+                return i;
+            }
+        }return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        for(int i=size()-1;i>=0;i--){
+            if(arrayList[i].equals(o))
+                return i;
+        }
+        return -1;
     }
 
     @Override
     public ListIterator<T> listIterator() {
+        //??????
         return null;
     }
 
     @Override
     public ListIterator<T> listIterator(int index) {
+        //??????????
         return null;
     }
 
     @Override
     public List<T> subList(int fromIndex, int toIndex) {
-        return null;
-    }
+        if(fromIndex < 0 || toIndex > size()){
+            throw new IndexOutOfBoundsException("index out of bounds");
+        }else if (fromIndex > toIndex){
+            throw new IllegalArgumentException("fromIndex must be lower than toIndex");
+        }else{
+            T [] array= Arrays.copyOfRange(arrayList, fromIndex,toIndex);
+            List<T> subList=Arrays.asList(array);
+            return subList;
+        }
 
-    @Override
-    public void replaceAll(UnaryOperator<T> operator) {
-
-    }
-
-    @Override
-    public void sort(Comparator<? super T> c) {
-
-    }
-
-    @Override
-    public Spliterator<T> spliterator() {
-        return null;
     }
 
 }
--- src/main/java/edu/codeschool/training/classes_objects/homework_17_Collection/TestArrayList.java
+++ src/main/java/edu/codeschool/training/classes_objects/homework_17_Collection/TestArrayList.java
@@ -1,20 +1,68 @@
 package edu.codeschool.training.classes_objects.homework_17_Collection;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 public class TestArrayList {
     public static void main(String[] args) {
-        ArrayList<String> str=new ArrayList<>();
-        str.add("A");
-        str.add("B");
-        str.add("C");
-        str.add("D");
-        str.add("E");
-        Iterator iter=str.iterator();
-        while(iter.hasNext()){
-            System.out.println(iter.next());
-        }
-        System.out.println(str.size());
+        ArrayList<Number> numbers=new ArrayList<>();
+        ArrayList<Double> doubles=new ArrayList<>();
+        ArrayList<Integer> integers=new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+//        doubles.add(5.6);
+//        doubles.add(3.2);
+//        doubles.add(3.25);
+//        doubles.add(2.678);
+
+//        integers.add(2);
+//        integers.add(4);
+//        System.out.println(numbers.addAll(1,doubles));
+//        System.out.println(numbers);
+//        System.out.println(numbers.size());
+
+//        System.out.println(numbers.removeAll(integers));
+//        System.out.println(numbers);
+//        System.out.println(numbers);
+//        numbers.clear();
+//        System.out.println(numbers);
+//        System.out.println(numbers.size());
+//                            numbers.remove(new Integer(1))   ;
+//                            numbers.remove(new Integer(2))   ;
+//                            numbers.remove(new Integer(3))   ;
+//                            numbers.remove(new Integer(4))   ;
+//                            numbers.remove(new Integer(5))   ;
+//        System.out.println(numbers);
+//        numbers.clear();
+//        System.out.println(numbers);
+//        System.out.println(numbers.size() );
+//        System.out.println(numbers.get(0) ) ;
+//        System.out.println(numbers.get(1) ) ;
+//        System.out.println(numbers.get(2) ) ;
+//        System.out.println(numbers.get(3) ) ;
+//        System.out.println(numbers.get(4) ) ;
+//        System.out.println(numbers.get(6) ) ;
+//        System.out.println(numbers.set(2,5));
+//        System.out.println(numbers);
+//        numbers.add(3,5);
+//        System.out.println(numbers);
+//        System.out.println(numbers.remove(0));
+//        System.out.println(numbers.remove(0));
+//        System.out.println(numbers.remove(0));
+//        System.out.println(numbers.lastIndexOf(7))   ;
+//        System.out.println(numbers.lastIndexOf(5))   ;
+        System.out.println(numbers.subList(0,5))  ;
+
+
+
+
+
     }
 }
