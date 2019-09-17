c9b29e31b4cecce784a7c4a6c140dd1b9674e6c5
gexayr <gexo7777@gmail.com>
2019-09-17 17:49:03 +0400

    homework17 LinkedList

--- src/edu/codeschool/training/homework17/client/TestLinkedList.java
+++ src/edu/codeschool/training/homework17/client/TestLinkedList.java
@@ -1,7 +1,9 @@
 package edu.codeschool.training.homework17.client;
 
-import java.util.LinkedList;
 import java.util.List;
+//import java.util.LinkedList;
+import edu.codeschool.training.homework17.exercise2.LinkedList;
+
 
 public class TestLinkedList {
     public static void main(String[] args) {
@@ -10,6 +12,27 @@ public class TestLinkedList {
         list.add("AA");
         list.add("BB");
         list.add("DD");
+//        list.remove("BB");
+            String s = list.get(1);
+        System.out.println(s);
+        System.out.println(list);
+
+
+
+//
+//
+//        String str1 = new String("Hello World!");
+//        String str2 = new String("My name is Earl");
+//        String str3 = new String("I love Java");
+//        String str4 = new String("I live in Moscow");
+//
+//        LinkedList<String> earlBio = new LinkedList<>();
+//        earlBio.add(str1);
+//        earlBio.add(str2);
+//        earlBio.add(str3);
+//        earlBio.add(str4);
+//
+//        System.out.println(earlBio);
 
     }
 }
--- src/edu/codeschool/training/homework17/exercise2/LinkedList.java
+++ src/edu/codeschool/training/homework17/exercise2/LinkedList.java
@@ -1,34 +1,35 @@
 package edu.codeschool.training.homework17.exercise2;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class LinkedList<E> implements List {
 
-//    private Node<E> head;
-//    private Node<E> tail;
-//    private int size = 0;
-//    private int position = 0;
-
-//    public LinkedList() {
-//        this.head = null;
-//        this.tail = null;
-//    }
+    private Node<E> head;
+    private Node<E> tail;
+    private int size = 0;
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
+        if(this.size == 0)
+            return true;
+
         return false;
     }
 
     @Override
     public boolean contains(Object o) {
+        Node<E> node = this.head;
+        for(int i = 0; i < this.size; i++){
+            if(node.equals(o))
+                return true;
+            node = node.getNext();
+        }
+
         return false;
     }
 
@@ -39,52 +40,156 @@ public class LinkedList<E> implements List {
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] array = new Object[this.size];
+        Node<E> node = this.head;
+        for(int i = 0; i < this.size; i++){
+            array[i] = node.getValue();
+            node = node.getNext();
+        }
+        return array;
     }
 
     @Override
     public boolean add(Object o) {
-        return false;
+        Node<E> nodeHead = this.head;
+        Node<E> nodeTail = this.tail;
+        Node<E> node = (Node<E>) new Node<>(o);
+        if (nodeHead == null){
+            this.head = node;
+        }else{
+            this.tail.setNext(node);
+            node.setPrevious(this.tail);
+        }
+
+        this.tail = node;
+        this.size++;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
+        Node<E> node = this.head;
+        while (node != this.tail) {
+            if (o != node.getValue()) {
+                node = node.getNext();
+            } else {
+                node.setValue(null);
+                if(o.equals(this.head)){
+                    node.setValue(null);
+                    this.head = node.getNext();
+                    node.getNext().setPrevious(null);
+                    node.setNext(null);
+                }else if(o.equals(this.tail)){
+                    node.setValue(null);
+                    this.tail = node.getPrevious();
+                    node.getPrevious().setNext(null);
+                    node.setPrevious(null);
+                }else{
+                    node.getPrevious().setNext(node.getNext());
+                    node.getNext().setPrevious(node.getPrevious());
+                }
+                this.size--;
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean addAll(Collection collection) {
-        return false;
+
+        for ( Object obj : collection ) {
+            this.add(obj);
+        }
+
+        return true;
     }
+
 
     @Override
     public boolean addAll(int i, Collection collection) {
-        return false;
+
+        for ( Object obj : collection ) {
+            this.add(i, obj);
+            i++;
+        }
+        return true;
     }
 
     @Override
     public void clear() {
-
+        for(int i = 0; i < this.size; i++){
+            this.remove(i);
+        }
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
     }
 
     @Override
-    public Object get(int i) {
-        return null;
+    public E get(int i) {
+        Node<E> node = this.head;
+        Node<E> nodeTail = this.tail;
+
+        if(i > (this.size / 2)){
+            int j = this.size;
+            while (j != i) {
+                node = nodeTail.getPrevious();
+                j--;
+            }
+        } else {
+            int j = 0;
+            while (j != i) {
+                node = node.getNext();
+                j++;
+            }
+        }
+        System.out.println(i);
+        System.out.println(node);
+        return (E) node;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(this.toArray());
     }
 
     @Override
     public Object set(int i, Object o) {
-        return null;
+        this.add(i, o);
+        return o;
     }
 
     @Override
     public void add(int i, Object o) {
 
+        Node<E> node;
+        Node<E> newNode = new Node<E>((E) o);
+
+            if (i == 0) {
+                newNode.setNext(this.head);
+                this.head.setPrevious(newNode);
+                this.head = newNode;
+            } else if(i == this.size){
+                newNode.setPrevious(this.tail);
+                this.tail.setNext(newNode);
+                this.tail = newNode;
+            } else {
+                node = (Node<E>) this.get(i);
+                newNode.setPrevious(node);
+                newNode.setNext(node.getNext());
+                node.getNext().setPrevious(newNode);
+                node.setNext(newNode);
+            }
+        this.size++;
     }
+
 
     @Override
     public Object remove(int i) {
-        return null;
+        Node<E> node = (Node<E>) this.get(i);
+        this.remove(node);
+        return node;
     }
 
     @Override
@@ -130,5 +235,61 @@ public class LinkedList<E> implements List {
     @Override
     public Object[] toArray(Object[] objects) {
         return new Object[0];
+    }
+}
+
+
+class Node<E> {
+    private E value;
+    private Node<E> next;
+    private Node<E> previous;
+
+    Node() {
+        this.value = null;
+        this.next = null;
+        this.previous = null;
+    }
+
+    Node(E value) {
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+
+    public Node(E value, Node<E> next, Node<E> previous) {
+        this.value = value;
+        this.next = next;
+        this.previous = previous;
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node<E> previous) {
+        this.previous = previous;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                '}';
     }
 }
